2f21b91bfed1085cb38c53d37cb0b50140f49a21
Akash Thaker <25161277+AkushMountains@users.noreply.github.com>
2019-03-09 20:01:30 -0500

    True MultiLateration Algorithm

---
+++ True_MultiLateration.py
@@ -0,0 +1,17 @@
+import math
+def true_multilateration(r1, r2, r3, U, Vx, Vy):
+   V = math.sqrt(Vx**2 + Vy**2)
+   x = (r1**2 - r2**2 + U**2)/(2*U)
+   y = (r1**2 - r3**2 + V**2 - (2 * Vx * x))/(2*Vy)
+   z = math.sqrt(r1**2 - x**2 - y**2)
+   return (x, y, z)
+
+r1 = float(input("Enter the r1:"))
+r2 = float(input("Enter the r2:"))
+r3 = float(input("Enter the r3:"))
+U = float(input("Enter the U:"))
+Vx = float(input("Enter the Vx:"))
+Vy = float(input("Enter the Vy:"))
+
+x,y,z = true_multilateration(r1,r2,r3,U,Vx,Vy)
+print(round(x,3), round(y,3),round(z,3))
