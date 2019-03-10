fceb1ade23bc94183581e6795918bfc82ed1b5aa
Akash Thaker <25161277+AkushMountains@users.noreply.github.com>
2019-03-09 21:01:33 -0500

    Edited a few changes in the input settings

--- True_MultiLateration.py
+++ True_MultiLateration.py
@@ -6,12 +6,12 @@ def true_multilateration(r1, r2, r3, U, Vx, Vy):
    z = math.sqrt(r1**2 - x**2 - y**2)
    return (x, y, z)
 
-r1 = float(input("Enter the r1:"))
-r2 = float(input("Enter the r2:"))
-r3 = float(input("Enter the r3:"))
-U = float(input("Enter the U:"))
-Vx = float(input("Enter the Vx:"))
-Vy = float(input("Enter the Vy:"))
+r1 = float(input("Enter the value of r1:"))
+r2 = float(input("Enter the value of r2:"))
+r3 = float(input("Enter the value of r3:"))
+U = float(input("Enter the value of U:"))
+Vx = float(input("Enter the value of Vx:"))
+Vy = float(input("Enter the value of Vy:"))
 
 x,y,z = true_multilateration(r1,r2,r3,U,Vx,Vy)
 print(round(x,3), round(y,3),round(z,3))
