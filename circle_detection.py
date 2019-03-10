3040ae6281f59b1791c9af0e170e114a80c854b3
rgj32 <rgj32@drexel.edu>
2019-03-09 21:02:39 -0500

    Circle OIR recognition

---
+++ circle_detection.py
@@ -0,0 +1,59 @@
+import numpy as np
+import argparse
+import cv2
+from PIL import Image
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+#ap.add_argument("-i", "--image", required=True, help="Path to the image")
+#args = vars(ap.parse_args())
+# load the image, clone it for output, and then convert it to grayscale
+image = cv2.imread("images/img_test_2.jpg")
+output = image.copy()
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray,(5,5),0);
+gray = cv2.medianBlur(gray,5)
+
+# Adaptive Guassian Threshold is to detect sharp edges in the Image. For more information Google it.
+gray = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,3.5)
+param2_num = 11
+circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.35, 100,
+                               param1=100,
+                               param2=10,
+                               minRadius=30,
+                               maxRadius=150
+                               )
+while circles.shape[1] != 3:
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.35, 100,
+                               param1=100,
+                               param2=param2_num,
+                               minRadius=35,
+                               maxRadius=150
+                               )
+    param2_num+=1
+    print(circles.shape)
+
+
+# ensure at least some circles were found
+if circles is not None:
+    # convert the (x, y) coordinates and radius of the circles to integers
+    circles = np.round(circles[0, :]).astype("int")
+
+
+    # loop over the (x, y) coordinates and radius of the circles
+    for (x, y, r) in circles:
+        # draw the circle in the output image, then draw a rectangle
+        # corresponding to the center of the circle
+
+
+
+        cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+        cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+
+    # show the output image
+    cv2.imshow("output", np.hstack([output]))
+    cv2.waitKey(0)
+
+
+
+
